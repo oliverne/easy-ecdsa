f201cfc95cd3922cb5956010c484b016af957ed3
Yun Cheolhun <oliverne@gmail.com>
2020-07-18 15:47:16 +0900

    Build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,47 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import rollupJSON from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'umd',
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      name: 'EasyECDSA',
+      file: 'dist/index.umd.js',
+      format: 'umd',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
+
   plugins: [
-    nodeResolve(),
+    nodeResolve({
+      browser: true,
+      preferBuiltins: false,
+    }),
     commonjs(),
+    rollupJSON(),
     typescript({
       rollupCommonJSResolveHack: true,
+    }),
+    terser({
+      output: {
+        comments: function (node, comment) {
+          var text = comment.value;
+          var type = comment.type;
+          if (type == 'comment2') {
+            return /@preserve|@license|@cc_on/i.test(text);
+          }
+        },
+      },
     }),
   ],
 };
